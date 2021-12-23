b48ced60dc382f7b71946d47adf787f5821b4d0a
sohui0103 <sohui0103@naver.com>
2021-12-23 20:24:24 +0900

    프로젝트 생성

---
+++ src/main/java/com/jojo/inflearn/intellij/Main.java
@@ -0,0 +1,7 @@
+package com.jojo.inflearn.intellij;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("d");
+    }
+}
---
+++ src/main/java/com/jojo/inflearn/intellij/Main2.java
@@ -0,0 +1,8 @@
+package com.jojo.inflearn.intellij;
+
+public class Main2 {
+    public static void main(String[] args) {
+        System.out.println("bbb");
+    }
+}
+
